9a6798e406bff084345b64e07dff2baabeceda76
Zeaphyou <76173678+Zeaphyou@users.noreply.github.com>
2023-02-19 23:44:38 +0800

    Update ConnectivityTest.js

--- script/ConnectivityTest.js
+++ script/ConnectivityTest.js
@@ -1,5 +1,5 @@
 
-# 由本群key修改提供。
+// 由本群key修改提供。
 const REQUEST_HEADERS = {
   'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
