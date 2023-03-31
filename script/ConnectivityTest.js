da87b4dc6b6ab5d74e3bbad425c700a8ab064735
Zeaphyou <76173678+Zeaphyou@users.noreply.github.com>
2023-03-31 15:29:23 +0800

    Update ConnectivityTest.js

--- script/ConnectivityTest.js
+++ script/ConnectivityTest.js
@@ -2,29 +2,22 @@
 let $ = {
 Bilibili:'https://www.bilibili.com',
 Baidu:'https://www.baidu.com',
-Youtube:'https://www.youtube.com/',
+YouTub:'https://www.youtube.com/',
 Google:'https://www.google.com/generate_204',
-Github:'https://www.github.com'
-}
-
+Github:'https://www.github.com'}
 !(async () => {
-await Promise.all([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('Youtube')]).then((x)=>{
+await Promise.all([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('YouTube')]).then((x)=>{
 	$done({
     title: 'Network Connectivity Test',
     content: x.join('\n'),
     icon: 'timer',
     'icon-color': '#FF5A9AF9',
-  })
-})
-})();
-
+  })})})();
 function http(req) {
     return new Promise((r) => {
 			let time = Date.now();
         $httpClient.post($[req], (err, resp, data) => {
             r(req +
-						'\xa0\xa0\xa0\t: ' +
+						'\xa0\xa0\xa0\xa0\t: ' +
 						(Date.now() - time)+' ms');
-        });
-    });
-}
+        });});}
