3ff0b72538f4b49c359ee098ad4dcdf037ce8af0
Zeaphyou <76173678+Zeaphyou@users.noreply.github.com>
2023-03-31 14:33:14 +0800

    Update ConnectivityTest.js

--- script/ConnectivityTest.js
+++ script/ConnectivityTest.js
@@ -10,14 +10,14 @@ Github:'https://www.github.com'
 }
 
 !(async () => {
-await  Promise.allSettled([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('Youtube')]).then(results => results.map(result => {
+await  Promise.allSettled([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('Youtube')]).then(results => {
  $done({
     title: 'Network Connectivity Test',
-    content: result.join('\n'),
+    content: results.map(result => result.value).join('\n'),
     icon: 'timer',
     'icon-color': '#FF5A9AF9',
   })
-}));
+});
 
 function http(req) {
     return new Promise((r) => {
