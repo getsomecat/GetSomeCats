16affc8154c1b1f0499bbe21d81c9030e1db34c4
Zeaphyou <zeaphyou@gmail.com>
2023-04-12 20:48:11 +0800

    Update CFGPT_2.js

--- modules/Panel/CFGPT/CFGPT_2.js
+++ modules/Panel/CFGPT/CFGPT_2.js
@@ -3,8 +3,9 @@
 自定义icon、iconerr及icon-color，利用argument参数传递，不同参数用&链接
 icon：支持☑️chatgpt时的图标，
 iconerr：不支持✖️chatgpt时的图标，
-icon-color：图标的颜色
-如：argument=icon=touchid&iconerr=xmark.seal.fill&icon-color=#D65C51
+icon-color：正常能使用时图标的颜色
+iconerr-color：不能使用时图标颜色
+如：argument=icon=lasso.and.sparkles&iconerr=xmark.seal.fill&icon-color=#336FA9&iconerr-color=#D65C51
 注⚠️：当想要自定义图标，必须要本地编辑，即保存在主配置中
 */
 
@@ -13,19 +14,21 @@ let tf=["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","B
 let tff=["plus","on"];
 
 // 处理 argument 参数
-let icon = '';
-let iconerr = '';
-let iconColor = '';
+let titlediy, icon, iconerr, iconColor, iconerrColor;
 if (typeof $argument !== 'undefined') {
   const args = $argument.split('&');
   for (let i = 0; i < args.length; i++) {
   const [key, value] = args[i].split('=');
-  if (key === 'icon') {
+  if (key === 'title') {
+    titlediy = value;
+  } else if (key === 'icon') {
     icon = value;
   } else if (key === 'iconerr') {
     iconerr = value;
   } else if (key === 'icon-color') {
     iconColor = value;
+  } else if (key === 'iconerr-color') {
+    iconerrColor = value;
   }
   }
 }
@@ -54,29 +57,31 @@ $httpClient.get(url, function(error, response, data){
   if (l !== -1) {
   gpt = "GPT: ✓";
   iconUsed = icon ? icon : undefined;
+  iconCol = iconColor ? iconColor : undefined;
+
   } else {
   gpt = "GPT: ✘";
   iconUsed = iconerr ? iconerr : undefined;
+  iconCol = iconerrColor ? iconerrColor : undefined;
+
   }
 
   // 获取 Warp 状态
   let w = tff.indexOf(warp);
-  let warpsCn;
+  let warps;
   if (w !== -1) {
-  warpsCn = "✓";
+  warps = "✓";
   } else {
-  warpsCn = "✘";
+  warps = "✘";
   }
 
   // 组装通知数据
   let body = {
-  title: "ChatGPT",
-  content: `${gpt}  Loc: ${loc}  Warp: ${warpsCn}`,
-  'icon-color': iconColor ? iconColor : undefined
+    title: titlediy ? titlediy : 'ChatGpT',
+    content: `${gpt}  Loc: ${loc}  Warp: ${warps}`,
+    icon: iconUsed ? iconUsed : undefined,
+    'icon-color': iconCol ? iconCol : undefined
   };
-  if (iconUsed) {
-  body['icon'] = iconUsed;
-  }
 
   // 发送通知
   $done(body);
